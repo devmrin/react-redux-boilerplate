9f2f3d75ff49d9f93c73a2283b65b90452eee6b6
Mrinmay <devmrin@gmail.com>
2018-10-14 19:56:29 +0530

    added provider

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
 import { createStore } from 'redux';
 
+//temporary - replace with actual reducer
+const rootReducer = state => state;
+
 const store = createStore(rootReducer);
 
 export default store;
