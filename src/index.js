4bd9c8e1997032313d30ee86abb4fd215388d005
Mrinmay <devmrin@gmail.com>
2018-10-14 19:43:30 +0530

    removed rest and added sanitize

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './styles/reset.scss';
+import './styles/sanitize.scss';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
